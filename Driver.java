d1e94b23f8a5053e44b5bc81d4ad7a3f0fadb713
cindyweng672 <cindyweng236@gmail.com>
2025-09-28 23:49:28 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,16 +1,35 @@
+
 public class Driver {
     public static void main(String[] args) {
-        Polynomial p = new Polynomial();
-        System.out.println(p.evaluate(3));
-        double[] c1 = { 6, 0, 0, 5 };
-        Polynomial p1 = new Polynomial(c1);
-        double[] c2 = { 0, -2, 0, 0, -9 };
-        Polynomial p2 = new Polynomial(c2);
-        Polynomial s = p1.add(p2);
-        System.out.println("s(0.1) = " + s.evaluate(0.1));
-        if (s.hasRoot(1))
-            System.out.println("1 is a root of s");
-        else
-            System.out.println("1 is not a root of s");
+        Polynomial emptyP = new Polynomial();
+
+        int[] redundExp = {1, 1, 1, 2, 5, 2, 9, 8};
+        double[] coef = {1, 1, 1.5, 2.5, 5, 2.5, 9, 8};
+        Polynomial p = new Polynomial(coef, redundExp);
+
+        int[] m1exp = {1, 3};
+        double[] m1coef = {2, 3};
+        int[] m2exp = {1, 3};
+        double[] m2coef = {3, 4};
+
+        double[] m3coef = {-1, 3};
+
+        Polynomial m1 = new Polynomial(m1coef, m1exp);
+        Polynomial m2 = new Polynomial(m2coef, m2exp);
+        Polynomial m3 = new Polynomial(m3coef, m2exp);
+        Polynomial product = m1.multiply(m3);
+
+        double[] m4coef = {1, 2, 2, 3};
+        double[] m5coef = {-1, -2, 2, -3};
+        int[] m4exp = {1, 2, 2, 3};
+        int[] m5exp = {1, 2, 2, 3};
+
+        Polynomial m4 = new Polynomial(m4coef, m4exp);
+        Polynomial m5 = new Polynomial(m5coef, m5exp);
+        Polynomial sum = m4.add(m5);
+
+        p.saveToFile("~\\Desktop\\CSCB07\\testPolynomial.txt");
+        product.saveToFile("~\\Desktop\\CSCB07\\testPolynomial.txt");
+        sum.saveToFile("~\\Desktop\\CSCB07\\testPolynomial.txt");
     }
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,62 +1,153 @@
+import java.io.*;
+import java.util.*;
 class Polynomial {
     private double[] coef;
+    private int[] exp;
 
     Polynomial() {
-        coef = new double[1];
-        coef[0] = 0;
+        coef = new double[0];
+        exp = new int[0];
     }
 
-    Polynomial(double[] tempCoef) {
-        int size = tempCoef.length;
-        coef = new double[size];
-        for (int i = 0; i < size; i++) {
-            coef[i] = tempCoef[i];
+    Polynomial(double[] coef, int[] exp) {
+        if(coef.length != exp.length){
+            return; 
         }
+        int size = coef.length;
+        int[] newExp = new int[size];
+        double[] newCoef = new double[size];
+        int currIndex = 0;
+        for(int i=0; i<size; i++){
+            int currExp = exp[i];
+            double currCoef = coef[i];
+            int expIndex = indexInArr(currExp, newExp);
+            if(expIndex ==-1){
+                newExp[currIndex] = exp[i];
+                newCoef[currIndex] = coef[i];
+                currIndex++;
+            }else{
+                newCoef[expIndex]+=coef[i];
+            }
+        }
+
+        this.exp = new int[currIndex];
+        this.coef = new double[currIndex];
+        for(int i=0; i<currIndex; i++){
+            this.exp[i]=newExp[i];
+            this.coef[i]=newCoef[i];
+        }
+    }
+
+    Polynomial(File f) throws IOException{
+        Scanner sc = new Scanner(f);
+        String line = sc.nextLine();
+
+        String[] tempArr = line.split("\\+|\\-");
+        int tempLen = tempArr.length;
+
+        double[] coef = new double[tempLen];
+        int[] exp = new int[tempLen];
+        int currIndex = 0;
+        for(int i=0; i<tempLen; i++){
+            double currCoef;
+            int currExp;
+            if(tempArr[i].contains("x")){
+                String[] currTerm = tempArr[i].split("x");
+                currCoef = Double.parseDouble(currTerm[0]);
+                if(currTerm.length == 1){
+                    currExp = 1;
+                }else{
+                    currExp = Integer.parseInt(currTerm[1]);
+                }
+            }else{
+                currCoef = Double.parseDouble(tempArr[0]);
+                currExp = 0;
+            }
+            int expIndex = indexInArr(currExp, exp);
+            if(expIndex == -1){
+                exp[currIndex] = currExp;
+                coef[currIndex] = currCoef;
+                currIndex++;
+            }else{
+                coef[expIndex] += currCoef;
+            }
+        }
+
+        this.exp = exp;
+        this.coef = coef;
     }
 
     double[] getCoef(){
         return coef;
     }
 
-    Polynomial padding(int lengthWanted) {
-        int currSize = coef.length;
-        double[] newCoef = new double[lengthWanted];
-        if (currSize < lengthWanted) {
-            int i;
-            for (i = 0; i < currSize; i++) {
-                newCoef[i] = coef[i];
+    int[] getExp(){
+        return exp;
+    }
+
+    int indexInArr(int target, int[] arr){
+        int size = arr.length;
+        for(int i=0; i<size; i++){
+            if(arr[i]==target){
+                return i;
             }
-            for (; i < lengthWanted; i++) {
-                newCoef[i] = 0;
-            }
-            return new Polynomial(newCoef);
         }
-        return new Polynomial(coef);
+        return -1;
     }
 
     Polynomial add(Polynomial p) {
         int argSize = p.coef.length;
         int currSize = coef.length;
-        int maxSize = Math.max(argSize, currSize);
 
-        double[] newCoef = new double[maxSize];
-        Polynomial newCurrPoly = padding(maxSize);
-        Polynomial newP = p.padding(maxSize);
-
-
-        for (int i = 0; i < maxSize; i++) {
-            newCoef[i] = newP.getCoef()[i] + newCurrPoly.getCoef()[i];
+        //create arrays with longest needed capacity
+        int longestPossibleLen = argSize+currSize;
+        int[] tempExp = new int[longestPossibleLen];
+        double[] tempCoef = new double[longestPossibleLen];
+        for(int i=0; i<argSize; i++){
+            tempExp[i]=p.getExp()[i];
+            tempCoef[i]=p.getCoef()[i];
+        }
+        
+        //fill the arrays
+        int lastValidIndex = argSize-1;
+        for(int i=0; i<currSize; i++){
+            int currExp = exp[i];
+            double currCoef = coef[i];
+            int expIndexInNew = indexInArr(currExp, tempExp);
+            if(expIndexInNew != -1){
+                tempCoef[expIndexInNew] += currCoef;
+            }else{
+                lastValidIndex++;
+                tempExp[lastValidIndex] = currExp;
+                tempCoef[lastValidIndex] = currCoef;
+            }
         }
 
-        return new Polynomial(newCoef);
+        //return the new Polynomial
+        int usedLen = lastValidIndex+1; 
+        int[] newExp = new int[usedLen];
+        double[] newCoef = new double[usedLen];
+        if(usedLen == longestPossibleLen){
+            newExp = tempExp;
+            newCoef = tempCoef;
+        }
+        else{
+            for(int i=0; i<usedLen; i++){
+                newExp[i]=tempExp[i];
+                newCoef[i] = tempExp[i];
+            }
+        }
+
+        return new Polynomial(newCoef, newExp);
     }
 
     double evaluate(double xVal){
-        int numCoef = coef.length;
-        double result = 0 ;
-        for(int i=0; i<numCoef; i++){
-            result += coef[i]*(Math.pow(xVal, i));
+        int size = coef.length;
+        double result = 0;
+        for(int i=0; i<size; i++){
+            result += coef[i]*(Math.pow(xVal, exp[i]));
         }
+
         return result;
     }
 
@@ -65,6 +156,82 @@ class Polynomial {
             return true;
         }else{
             return false;
+        }
+    }
+
+    Polynomial multiply(Polynomial p){
+        int currPolySize = coef.length;
+        int pPolySize = p.getCoef().length;
+
+        //create an array with longest needed capacity
+        int longestPossibleLen = currPolySize * pPolySize;
+        double[] tempCoefArr = new double[longestPossibleLen];
+        int[] tempExpArr = new int[longestPossibleLen];
+        
+        //fill the new arrays with corresponding values
+        int currIndex = 0;
+        for(int i=0; i<currPolySize; i++){
+            for(int j=0; j<pPolySize; j++){
+                double newCoef = coef[i] * p.getCoef()[j];
+                int newExp = exp[i]+p.getExp()[j];
+
+                int newExpIndex = indexInArr(newExp, tempExpArr);
+                if(newExpIndex==-1){
+                    tempCoefArr[currIndex] = newCoef;
+                    tempExpArr[currIndex] = newExp;
+                    currIndex++;
+                }else{
+                    tempCoefArr[newExpIndex] += newCoef;
+                }
+            }
+        }
+
+        //return the new Polynomial 
+        if(currIndex == longestPossibleLen-1){
+            return new Polynomial(tempCoefArr, tempExpArr);
+        }else{
+            //the new Polynomial exclude unused elements 
+            int[] newExpArr = new int[currIndex+1];
+            double[] newCoefArr = new double[currIndex+1];
+            for(int i=0; i<=currIndex; i++){
+                newExpArr[i] = tempExpArr[i];
+                newCoefArr[i] = tempCoefArr[i];
+            }
+            return new Polynomial(newCoefArr, newExpArr);
+        }
+    }
+
+    void saveToFile(String filePath){
+        File f = new File(filePath);
+        String polyString = "";
+        int len = exp.length;
+        for(int i=0; i<len; i++){
+            if(coef[i]>=0 && polyString==""){
+                polyString+=coef[i];
+            }else{
+                if(coef[i]>=0){
+                    polyString += "+";
+                    polyString += coef[i];
+                }else{
+                    polyString += coef[i];
+                }
+            }
+            if(coef[i]==0){
+                continue;
+            }else{
+                polyString+="x";
+                if(coef[i]==1){
+                    continue;
+                }
+                polyString+=exp[i];
+            }
+        }
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(f))){
+            f.createNewFile();
+            bw.write(polyString);
+
+        } catch (Exception e) {
+            System.out.println("save file error");
         }
     }
 }
